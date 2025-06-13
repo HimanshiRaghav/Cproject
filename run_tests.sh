9b3cf3b2456ccc8f7fa646a135e710c142b778c1
HimanshiRaghav <142144235+HimanshiRaghav@users.noreply.github.com>
2025-06-13 21:26:30 +0530

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,25 +1,25 @@
-# #!/bin/bash
+#!/bin/bash
 
-# echo "Running test_exec..."
+echo "Running test_exec..."
 
-# # Read build system from env var set by workflow input
-# if [ "$BUILD_SYSTEM" == "cmake" ]; then
-#   if [ -f "./build/test_exec" ]; then
-#     chmod +x ./build/test_exec
-#     ./build/test_exec
-#   else
-#     echo "test_exec not found in build directory!"
-#     exit 1
-#   fi
-# elif [ "$BUILD_SYSTEM" == "make" ]; then
-#   if [ -f "./test_exec" ]; then
-#     chmod +x ./test_exec
-#     ./test_exec
-#   else
-#     echo "test_exec not found in root directory!"
-#     exit 1
-#   fi
-# else
-#   echo "Unknown build system: $BUILD_SYSTEM"
-#   exit 1
-# fi
+# Read build system from env var set by workflow input
+if [ "$BUILD_SYSTEM" == "cmake" ]; then
+  if [ -f "./build/test_exec" ]; then
+    chmod +x ./build/test_exec
+    ./build/test_exec
+  else
+    echo "test_exec not found in build directory!"
+    exit 1
+  fi
+elif [ "$BUILD_SYSTEM" == "make" ]; then
+  if [ -f "./test_exec" ]; then
+    chmod +x ./test_exec
+    ./test_exec
+  else
+    echo "test_exec not found in root directory!"
+    exit 1
+  fi
+else
+  echo "Unknown build system: $BUILD_SYSTEM"
+  exit 1
+fi
