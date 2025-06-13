55981a198716bbe4c22d14f0c93ba5da12129ad6
HimanshiRaghav <142144235+HimanshiRaghav@users.noreply.github.com>
2025-06-13 20:55:22 +0530

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Running test_exec..."
-if [ -f "./test_exec" ]; then
-  chmod +x ./test_exec
-  ./test_exec
+if [ -f "./build/test_exec" ]; then
+  chmod +x ./build/test_exec
+  ./build/test_exec
 else
   echo "test_exec not found!"
   exit 1
