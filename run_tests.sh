dd6b20e2b68b1d3c21b803abfa9a83677344dca1
HimanshiRaghav <142144235+HimanshiRaghav@users.noreply.github.com>
2025-06-13 21:31:52 +0530

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -6,14 +6,14 @@ echo "Running test_exec..."
 if [ "$BUILD_SYSTEM" == "cmake" ]; then
   if [ -f "./build/test_exec" ]; then
     chmod +x ./build/test_exec
-    ./build/test_execi
+    ./build/test_exec
   else
     echo "test_exec not found in build directory!"
     exit 1
   fi
 elif [ "$BUILD_SYSTEM" == "make" ]; then
   if [ -f "./test_exec" ]; then
-    chmod +x ./test_execi
+    chmod +x ./test_exec
     ./test_exec
   else
     echo "test_exec not found in root directory!"
